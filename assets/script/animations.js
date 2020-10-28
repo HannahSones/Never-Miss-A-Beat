3da5431b5261b6d9d038ce29b672a7e2104f3bdc
PavN93 <paweln993@gmail.com>
2020-10-28 12:18:16 +0000

    Added initial files with some HTML and CSS template

---
+++ assets/script/animations.js
@@ -0,0 +1,16 @@
+const toTopButton = $("[data-scroll='top']");
+const headerWithNav = $("header");
+const mainSection = $("section");
+const stickyFooter = $("footer")
+
+$(toTopButton).on("click", function () {
+  $("html, body").animate({
+    scrollTop: 0
+  }, "slow")
+});
+
+$(headerWithNav).fadeIn(800, function() {
+  $(mainSection).fadeIn(800, function() {
+    $(stickyFooter).fadeIn(500)
+  })
+});
