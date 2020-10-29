0287d59cabee66857954e42095648edd4277f9b8
Pawel <paweln993@gmail.com>
2020-10-29 21:19:37 +0000

    Updated layout

--- assets/script/animations.js
+++ assets/script/animations.js
@@ -1,16 +1,6 @@
-const toTopButton = $("[data-scroll='top']");
 const headerWithNav = $("header");
-const mainSection = $("section");
 const stickyFooter = $("footer")
 
-$(toTopButton).on("click", function () {
-  $("html, body").animate({
-    scrollTop: 0
-  }, 0)
-});
-
-$(headerWithNav).fadeIn(800, function() {
-  $(mainSection).fadeIn(800, function() {
-    $(stickyFooter).fadeIn(500)
-  })
+$(headerWithNav).fadeIn(200, function() {
+    $(stickyFooter).fadeIn(200)
 });
