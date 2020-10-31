2aface88a417e69d02a17ca36c4474f1f77346bb
Pawel <paweln993@gmail.com>
2020-10-31 13:58:49 +0000

    Code refactor, file structure update, search button wired up

---
+++ assets/script/main.js
@@ -0,0 +1,45 @@
+const searchBtn = $("#search-btn");
+const searchInputText = $("#input_text");
+const lastfmAPIkey = "6a57108ac7001b783396597bbb4b2c61"
+
+
+// Load the events carousel
+function loadEventsCarousel() {
+  $('.carousel').carousel();
+};
+
+loadEventsCarousel();
+
+
+// Get object throgh ajax
+function getSearchResults(queryURL) {
+  $.ajax({
+      url: queryURL
+  }).then(function(response) {
+      console.log(response);
+  }).catch(function(){
+      console.log("error error");
+  })
+};
+
+// Search Button event listener
+$(searchBtn).on("click", function() {
+    constructArtistSearchURL(8, lastfmAPIkey);
+    historySearchBtnClicked();
+});
+
+//set local storage 
+function setLocalStorage() {
+  const searchHistory = getFromLocalStorage()
+  searchHistory.push( /* val from search variable */ )
+  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+};
+
+//get from local storage
+function getFromLocalStorage() {
+  const storedSearchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+  if (storedSearchHistoy && Array.isArray(storedSearchHistory) && storedSearchHistory.length >= 1) {
+      return storedSearchHistory;
+  }
+  return [];
+}
