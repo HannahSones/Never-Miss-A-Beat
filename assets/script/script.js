fd116e1ca45e0a5e1214aad4629c0671b27e567f
Mark Leonard <marketh@me.com>
2020-10-31 11:33:38 +0000

    Added HTML for search history. Started on the JS

---
+++ assets/script/script.js
@@ -0,0 +1,30 @@
+//Search Histor
+
+
+let searchHistory = [];
+
+
+//get from local storage
+function getFromLocalStorage() {
+    const storedSearchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    if (storedSearchHistoy && Array.isArray(storedSearchHistory) && storedSearchHistory.length >= 1) {
+        return storedSearchHistory;
+    }
+    return [];
+}
+
+//set local storage 
+function setLocalStorage() {
+    const searchHistory = getFromLocalStorage()
+
+
+    searchHistory.push( /* val from search variable */ )
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+}
+//display search history function
+function displaySearchHistory() {
+    const getSearchHistory = getFromLocalStorage();
+
+
+}
+//event handler on search history list
