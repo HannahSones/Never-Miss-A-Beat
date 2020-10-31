91fa954b4285a6fc28f10e34ef917d90acbb52d6
Hannah Sones <hannah.sones@outlook.com>
2020-10-31 10:51:26 +0000

    HTML written for events template with carousel function in script file

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+// Load the events carousel
+function loadEventsCarousel(){
+    $('.carousel').carousel();
+    };
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+
+    loadEventsCarousel();
+
+
+}); 
